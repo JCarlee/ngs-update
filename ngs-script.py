e61dda99573c67582f081c05c3d631cb1e9380e6
jcarlee <jcarlee@gmail.com>
2018-07-10 18:35:30 -0500

    basically working2

--- ngs-script.py
+++ ngs-script.py
@@ -4,8 +4,14 @@
 # Copy previous "new" files into "old"
 
 import os
+import datetime
+now = datetime.datetime.now()
 
 root = 'E:\\Planet15JCarlee\\NGS'
 
 dirlist = [item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item))]
-print(max(dirlist))
+
+prev_month = max(dirlist)
+prev_dir = root + '\\' + prev_month
+new_dir = "{0}\\{1}{2}{3}".format(root, now.year, '{:02d}'.format(now.month), '{:02d}'.format(now.day))
+print(new_dir)
