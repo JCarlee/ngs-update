643c954db771044ed694b4256125556ad5ccba07
jcarlee <jcarlee@gmail.com>
2018-07-10 16:56:03 -0500

    basically working

--- ngs-script.py
+++ ngs-script.py
@@ -3,3 +3,9 @@
 # Create new folder \new \old
 # Copy previous "new" files into "old"
 
+import os
+
+root = 'E:\\Planet15JCarlee\\NGS'
+
+dirlist = [item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item))]
+print(max(dirlist))
