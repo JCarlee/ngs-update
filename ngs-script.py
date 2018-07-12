5fa774002d4801dcc18dab61bcdef59fdc8c4c63
jcarlee <jcarlee@gmail.com>
2018-07-12 14:28:17 -0500

    cleanup

--- ngs-script.py
+++ ngs-script.py
@@ -22,39 +22,35 @@ class MyClass(object):
         self.source = os.listdir(self.prev_dir_new)                       # Create list from previous month's
 
     def dir_move(self):
-        # Create new month root directory
-        if not os.path.exists(self.new_dir):
+        if not os.path.exists(self.new_dir):        # Create new month root directory
             os.makedirs(self.new_dir)
 
-        # Create new-new directory
-        if not os.path.exists(self.new_dir_new):
+        if not os.path.exists(self.new_dir_new):    # Create new-new directory
             os.makedirs(self.new_dir_new)
             print("CREATED: " + self.new_dir_new)
 
-        # Create new-old directory
-        if not os.path.exists(self.new_dir_old):
+        if not os.path.exists(self.new_dir_old):    # Create new-old directory
             os.makedirs(self.new_dir_old)
             print("CREATED: " + self.new_dir_old)
 
-        # Copy old dbf files into new-old directory
-        for files in self.source:
+        for files in self.source:                   # Copy old dbf files into new-old directory
             if files.endswith(".dbf"):
                 shutil.copy(self.prev_dir_new + '\\' + files, self.new_dir_old)
 
         print("Copied .dbf files from {0} to {1}".format(self.prev_dir_new, self.new_dir_old))
 
-    def ftp_dl(self):  # Function to download zip files from FTP
+    def ftp_dl(self):                               # Function to download zip files from FTP
         ftp = FTP("ftp.ngs.noaa.gov", "anonymous")  # Define FTP credentials
         ftp.login()                                 # Login using credentials
         ftp.cwd("/pub/DS_ARCHIVE/ShapeFiles")       # Change directory to shapefiles
         root_dirs = ftp.nlst()                      # Create list of files in FTP directory
         root_dirs.remove('ARCHIVE')                 # Remove folder 'ARCHIVE' from the list
 
-        for i in root_dirs:  # Loop through items in FTP dir, downloading each to new-new dir
+        for i in root_dirs:                         # Loop through items in FTP dir, downloading each to new-new dir
             print(i + " downloading " + str(ftp.size(i)) + " bytes")
             local_filename = os.path.join(self.new_dir_new, i)
             fh = open(local_filename, 'wb')
-            ftp.retrbinary('RETR ' + i, fh.write)  # Download current file in loop
+            ftp.retrbinary('RETR ' + i, fh.write)   # Download current file in loop
             print(i + " download complete")
         ftp.close()
 
